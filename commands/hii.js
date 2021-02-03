014f19582c9d99d7756801e988ed8281b759254c
Ayushverma408 <ayushverma462002@gmail.com>
2021-02-03 18:11:37 +0530

    First command added

---
+++ commands/hii.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: 'hii',
+    description: "this is a Hii",
+    execute(message, args){
+        message.channel.send('hewoo!');
+    }
+}
--- index.js
+++ index.js
@@ -3,9 +3,17 @@ require('dotenv').config();
 
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const fs = require('fs');
+const prefix = '-';
 
+client.commands = new Discord.Collection();
 
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+for(const file of commandFiles){
+	const command = require(`./commands/${file}`);
 
+	client.commands.set(command.name, command);
+}
 
 // Setting up status presence and logging ready when on.
 
@@ -17,41 +25,25 @@ client.once('ready', () => {
 		activity: {
 			name: 'KALM',
 			type: 'PLAYING'
-		}
-	})
+					}
+		})
 });
-
-
 
 
 //Client.on is Inititated here
 
-client.on('message', message => {
-	console.log(message.content);
+client.on('message', message =>{
 
-if (message.content === 'Moron') {
-	// send back "Pong." to the channel the message was sent in
-    message.channel.send('No U Stoopid');
-}
+	if(!message.content.startsWith(prefix) || message.author.bot) return;
 
-if (message.content === 'Cool') {
-	// send back "Pong." to the channel the message was sent in
-    message.channel.send('Yes very');
-}
+	const args = message.content.slice(prefix.length).split(/ +/);
+	const command = args.shift().toLowerCase();
 
-if (message.content === 'Damn!') {
-	// send back "Pong." to the channel the message was sent in
-    message.channel.send('Not yashu talking to a bot like talking talking');
-}
-
-if (message.content === 'ooof') {
-	// send back "Pong." to the channel the message was sent in
-    message.channel.send('Shut.... SHUSH');
-    //just a change
-}
+	if(command === 'hii'){
+		client.commands.get('hii').execute(message, args);
+		
+	}
 });
-
-
 
 // END OF Index.js
 client.login(process.env.TOKEN);
