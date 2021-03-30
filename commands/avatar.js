b6504cf4a87ad72b3612f6bee1c84aa0138130c6
Ayushverma408 <ayushverma462002@gmail.com>
2021-03-30 20:39:12 +0530

    avatar command fully functional

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,14 +1,27 @@
+const Discord = require("discord.js");
 module.exports = {
     name: 'avatar',
     description: "use this command to preview your avatar inn higher resolution",
-    execute(message, args){
+    async execute(message, args){
+    
+    let msg = await message.channel.send("Generating avatar...");
 
-        message.channel.send(` ${message.author} Pomodoro started for ${args} min! \n Go work now :3`);
-        message.react('🍅');
-        function myFunc(arg) {
-            console.log(`arg was => ${arg}`);
-            message.channel.send(`${message.author} Pomodoro finished \nHave some rest now :>`);
-          }
-          setTimeout(myFunc, 1000*args*60, 'finished'); 
+    if(message.mentions.users.size){
+        let member=message.mentions.users.first()
+            
+        if(member){
+            const emb=new Discord.MessageEmbed().setImage(member.displayAvatarURL({size: 2048})).setTitle(member.username)
+            message.channel.send(emb)
+                
+        }
+        else{
+            message.channel.send("Sorry none found with that name")
+    
+        }
+        }else{
+            const emb=new Discord.MessageEmbed().setImage(message.author.displayAvatarURL({size: 2048})).setTitle(message.author.username)
+            message.channel.send(emb)
+            }
+            msg.delete();
 }
 }
