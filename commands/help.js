401dd5b7806e6d7617c193d831f384d17c2e67e1
Ayushverma408 <ayushverma462002@gmail.com>
2021-04-01 17:45:34 +0530

    brrrrrrrrrrb help in index

--- commands/help.js
+++ commands/help.js
@@ -19,3 +19,18 @@ module.exports.run = async(message ) => {
 
     });
 }
+
+module.exports = {
+    name: 'help',
+    description: "brrrrrrrrb",
+    async execute(message, args){
+
+        message.channel.send(` ${message.author} Pomodoro started for ${args} min! \n Go work now :3`);
+        message.react('🍅');
+        function myFunc(arg) {
+            console.log(`arg was => ${arg}`);
+            message.channel.send(`${message.author} Pomodoro finished \nHave some rest now :>`);
+          }
+          setTimeout(myFunc, 1000*args*60, 'finished'); 
+}
+}
