ca3ae43a7ed13b28cccfc113105080693c1869c3
Ayushverma408 <ayushverma462002@gmail.com>
2021-04-01 17:45:45 +0530

    help in index

--- index.js
+++ index.js
@@ -110,7 +110,27 @@ client.on('message', async message =>{
 
 	//help command
 	if(command === 'help'){
-		client.commands.get('help').execute(message, args);
+		module.exports.run = async(message ) => {
+			fs.readdir("./commands/", (err, files) => {
+				if(err) console.error(err);
+		
+				let jsfiles = files.filter(f => f.split(".").pop() === "js");
+				if(jsfiles.length <= 0) {
+					console.log("No commands to load!");
+					return;
+				}
+		
+		
+				let result = jsfiles.forEach((f, i) => {
+					let props = require(`./${f}`);
+					let filesArray = [props.help.name, props.help.description]
+					//let filesArray = [props.help.name, props.help.description, props.help.usage]
+					message.author.send(`**${filesArray[0]}** \n${filesArray[1]}`);
+					//message.author.send(`**${filesArray[0]}** \n${filesArray[1]} \n${filesArray[2]}`);
+				});
+		
+			});
+		}
 		
 	}
 
