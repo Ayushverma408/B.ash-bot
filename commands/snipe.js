501a51ac13d680e61e51edb8882c8dcb27969f35
Ayushverma408 <ayushverma462002@gmail.com>
2021-05-19 05:02:32 +0530

    snipe!! removed async

--- commands/snipe.js
+++ commands/snipe.js
@@ -3,8 +3,11 @@ const { Message, MessageEmbed } = require('discord.js');
 module.exports = {
     name: 'snipe',
     description: "get sniped lol",
-    async execute(message, args){
+    execute(message, args){
         const msg = bot.snipes.get(message.channel.id)
+
+        if (!msg) return message.channel.send(`That is not a valid snipe...`);
+        
         const embed = new Discord.MessageEmbed()
         .setAuthor(msg.author, msg.member.user.displayAvatarURL())
         .setDescription(msg.content)
