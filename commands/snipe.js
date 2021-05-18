2474c1ac7c9cd9a42d5a6ada5eedb551e5a24ca2
Ayushverma408 <ayushverma462002@gmail.com>
2021-05-19 05:11:10 +0530

    snipe!! bot replace by client

--- commands/snipe.js
+++ commands/snipe.js
@@ -1,18 +1,19 @@
-const { Message, MessageEmbed } = require('discord.js');
+const { Message, MessageEmbed, Client } = require('discord.js');
 
 module.exports = {
     name: 'snipe',
     description: "get sniped lol",
-    execute(message, args){
-        const msg = bot.snipes.get(message.channel.id)
+    async execute(message, args){
+        const msg = client.snipes.get(message.channel.id)
 
         if (!msg) return message.channel.send(`That is not a valid snipe...`);
-        
+
         const embed = new Discord.MessageEmbed()
         .setAuthor(msg.author, msg.member.user.displayAvatarURL())
         .setDescription(msg.content)
         .setFooter('Get Sniped lol')
         .setTimestamp();
+        if (msg.image) embed.setImage(msg.image);
         message.channel.send(embed);
     }
         // message.channel.send(answer.definition)
